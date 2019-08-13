2ebf55350021b18e7fe05b9e2d4acbfcdac8a54c
michael <willmichael@tozny.com>
2019-08-13 13:06:12 -0700

    karma travis config

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,11 @@ module.exports = function(config) {
     logLevel: config.LOG_WARN,
 
     sauceLabs: {
-      testName: 'Karma and Sauce Labs demo'
+      testName: 'Karma and Sauce Labs demo',
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+      startConnect: false
     },
     captureTimeout: 120000,
     customLaunchers: customLaunchers,
